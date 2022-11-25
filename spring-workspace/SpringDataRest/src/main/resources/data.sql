75e45b9dfeff8d5201117b48fcb27c0fff853fd8
ajmins <ajmi.ns@inapp.com>
2022-11-25 17:27:34 +0530

    5th commit: spring_workspace files added

---
+++ spring-workspace/SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into Alien values (2,'Anaya','Php');
+INSERT into Alien values (1,'Ajimi','Java');
+INSERT into Alien values (7,'Mita','Php');
+INSERT into Alien values (3,'Dona','Java');
+INSERT into Alien values (4,'Ether','iOS');
+INSERT into Alien values (10,'Tina','Java');
